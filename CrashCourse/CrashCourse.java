1f3818ec8f99de9c3904a22a065bd8b3d915b428
Nebra77 <arbenhusenaj09@gmail.com>
2026-09-02 12:35:26 +0000

    Crash Course Stuff

---
+++ CrashCourse/CrashCourse.java
@@ -0,0 +1,66 @@
+public class CrashCourse {     // class header
+
+    public static void main(String[] args) {
+
+        // comments 
+        
+        /*  Multiline 
+         Comment
+         Here
+         */
+
+         System.out.print("Hello");
+         System.out.println("World"); // auto-prints on a new line
+         System.out.println("Hello world");
+
+         // varialbe declarations:
+         // PRIMITIVE VARIABLES
+         int a; // integer
+         double b; // decimal, or float
+         boolean c; // true or false;
+
+         a = 4;
+         b = 5.5;
+         c = false; // lowercase
+
+         // Arithmetic Operators 
+         // + - / *  %
+         // += -= /=  %=
+
+        int d = 3;
+        d += 7;
+
+        System.out.println("d is equal to " + d);
+
+        // increment and decrement by one:
+        // --  ++
+        d--;
+        d++;
+        d++;
+        System.out.println("d is NOW equal to " + d);
+
+        //COMPARISONS (always returns true or false)
+        //  <  >   <=   >=   ==   !=
+        System.out.println(4 < 5);
+        System.out.println(7 == 4);
+        System.out.println(1 != 2);
+
+        //LOGICAL OPERATORS  
+        // ! not
+        // %% and
+        // || or
+        
+/*
+true
+false
+true
+true */
+
+
+
+
+
+
+
+    }
+}
