ae1a1919b8cebde6e241235a8e15c64c6a0ab18d
Nebra77 <arbenhusenaj09@gmail.com>
2026-09-15 12:39:40 +0000

    otherpart

---
+++ CrashCourse/JarranMohn.java
@@ -0,0 +1,46 @@
+public class JarranMohn {
+    String characterNameString;
+    int health;
+    int defence;
+    int strength;
+    int speed;
+    int luck;
+    int intelligence;
+    int charisma;
+    int terrariaSkill;
+    int attractiveness;
+    public JarranMohn(String characterName, int terrariaSkill, int strength){
+        this.characterName = characterName;
+        this.terrariaSkill = terrariaSkill;
+        this.strength = strength;
+    }
+    public void attack(){
+        System.out.println("attacking");
+    }
+    public void move(){
+        System.out.println("moving");
+    }
+    public void playTerraria(int hours){
+        terrariaSkill += hours;
+    }
+    public void fence(int hours){
+        strength += hours;
+    }
+    public void talkToArben(int hours){
+        charisma += hours;
+    }
+    public void learnGerman(int hours){
+        intelligence += hours;
+    }
+    public void dontPlayTerraira(){
+        health-=9999;
+        defence-=9999;
+        strength-=9999;
+        speed-=9999;
+        luck-=9999;
+        intelligence-=9999;
+        terrariaSkill-=9999;
+        attractiveness-=9999;
+        charisma-=9999;
+    }
+}
